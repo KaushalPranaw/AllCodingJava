64059011d515319bf1ca9d07eb5cf9c4d87b6d65
KaushalPranaw <pranaw.kaushal.1997@gmail.com>
2026-01-07 21:34:24 +0530

    Jan 7, 2025

--- src/Leetcode/Graph/G03_Topo/G17_TopoSortKahnAlgo.java
+++ src/Leetcode/Graph/G03_Topo/G17_TopoSortKahnAlgo.java
@@ -1,9 +1,6 @@
 package Leetcode.Graph.G03_Topo;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.*;
 
 public class G17_TopoSortKahnAlgo {
     public static void main(String[] args) {
@@ -27,41 +24,33 @@ public class G17_TopoSortKahnAlgo {
     }
 
     private static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) {
-        int[] indegree = new int[V];
-        for (int i = 0; i < V; i++) {
-            for (int it : adj.get(i)) {
+        int[] indegree=new int[V];
+
+        for(int i=0;i<V;i++){
+            for(int it: adj.get(i)){
                 indegree[it]++;
             }
         }
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < V; i++) {
-            if (indegree[i] == 0) {
+
+        Queue<Integer> queue=new LinkedList<>();
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0){
                 queue.add(i);
             }
         }
-        int[] topo = new int[V];
-        int index = 0;
 
-        while (!queue.isEmpty()) {
-            topo[index++] = queue.peek();
-            int node = queue.poll();
-            for (int it : adj.get(node)) {
+        List<Integer> topo=new ArrayList<>();
+        while (!queue.isEmpty()){
+            int node=queue.poll();
+            topo.add(node);
+
+            for(int it: adj.get(node)){
                 indegree[it]--;
-                if (indegree[it] == 0) {
+                if(indegree[it]==0){
                     queue.add(it);
                 }
             }
         }
-        return topo;
-    }
-
-    private static void dfs(int node, ArrayList<ArrayList<Integer>> adj, int[] vis, Stack<Integer> stack) {
-        vis[node] = 1;
-        for (int it : adj.get(node)) {
-            if (vis[it] == 0) {
-                dfs(it, adj, vis, stack);
-            }
-        }
-        stack.push(node);
+        return topo.stream().mapToInt(i->i).toArray();
     }
 }
--- src/Leetcode/Graph/G03_Topo/G22_AlienDictionary.java
+++ src/Leetcode/Graph/G03_Topo/G22_AlienDictionary.java
@@ -21,59 +21,57 @@ public class G22_AlienDictionary {
     }
 
     public String findOrder(String[] dict, int N, int K) {
-        List<List<Integer>> adjList = new ArrayList<>();
+        List<List<Integer>> adj = new ArrayList<>();
         for (int i = 0; i < K; i++) {
-            adjList.add(new ArrayList<>());
+            adj.add(new ArrayList<>());
         }
 
-        for (int i = 0; i < N - 1; i++) {
+        for (int i = 0; i < dict.length - 1; i++) {
             String s1 = dict[i];
             String s2 = dict[i + 1];
             int len = Math.min(s1.length(), s2.length());
-            for (int ptr = 0; ptr < len; ptr++) {
-                if (s1.charAt(ptr) != s2.charAt(ptr)) {
-                    adjList.get(s1.charAt(ptr) - 'a').add(s2.charAt(ptr) - 'a');
+            for (int x = 0; x < len; x++) {
+                if (s1.charAt(x) != s2.charAt(x)) {
+                    adj.get(s1.charAt(x) - 'a').add(s2.charAt(x) - 'a');
                     break;
                 }
             }
         }
 
-        List<Integer> topo = topoSort(K, adjList);
-        StringBuilder ans = new StringBuilder();
-        for (int it : topo) {
-            ans.append((char) (it + 'a'));
+        List<Integer> topo = topoSort(K, adj);
+        StringBuilder sb = new StringBuilder();
+        for (int i : topo) {
+            sb.append((char) (i + 'a'));
         }
-        return ans.toString();
+        return sb.toString();
     }
 
     private List<Integer> topoSort(int V, List<List<Integer>> adj) {
-        int indegree[] = new int[V];
+        int[] indegree = new int[V];
         for (int i = 0; i < V; i++) {
             for (int it : adj.get(i)) {
                 indegree[it]++;
             }
         }
 
-        Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> queue = new LinkedList<>();
         for (int i = 0; i < V; i++) {
             if (indegree[i] == 0) {
-                q.add(i);
+                queue.add(i);
             }
         }
-        List<Integer> topo = new ArrayList<>();
-        while (!q.isEmpty()) {
-            int node = q.peek();
-            q.remove();
-            topo.add(node);
-            // node is in your topo sort
-            // so please remove it from the indegree
 
+        List<Integer> topo = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            topo.add(node);
             for (int it : adj.get(node)) {
                 indegree[it]--;
-                if (indegree[it] == 0) q.add(it);
+                if (indegree[it] == 0) {
+                    queue.add(it);
+                }
             }
         }
-
         return topo;
     }
 }
